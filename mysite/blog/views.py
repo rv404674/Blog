7f0159f903cb90645c75ccca987537f1b9739338
Rahul Verma <rv404674@gmail.com>
2018-10-16 04:19:22 +0530

    Added code to recommend post based on tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -54,9 +55,17 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+
+    # List of similar post
+    # value_list returns value for given fields
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 
